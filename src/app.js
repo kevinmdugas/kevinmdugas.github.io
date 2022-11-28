917cddde32df485dc18221ac289476ff2047682c
dev <kdugas@pdx.edu>
2022-11-27 18:12:14 -0800

    Navbar works with icon

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
 const renderNav = (title) => {
   const body = document.querySelector("body");
 
-  const navContainer = document.createElement("container");
+  const navContainer = document.createElement("flex-container");
   const navRow = document.createElement("div");
   navRow.className = "row py-2 px-3";
 
-  // const icon = document.createElement('img')
-  // icon.className = "col-1"
+  const iconLink = document.createElement("a");
+  iconLink.className = "col-auto mt-1";
+  iconLink.href = "index.html";
+
+  const icon = document.createElement("img");
+  icon.src = "assets/coffee.svg";
+  icon.style.maxWidth = "100%";
+
   const name = document.createElement("a");
-  name.className = "col-4 fs-3 fw-bold text-decoration-none text-primary";
+  name.className =
+    "col-auto px-0 fs-3 fw-bold text-decoration-none text-primary";
   name.textContent = "Kevin Dugas";
   name.href = "index.html";
 
   const navbar = document.createElement("ul");
-  navbar.className = "col-8 nav nav-pills justify-content-end navbar-light";
+  navbar.className = "col nav nav-pills justify-content-end navbar-light";
 
   const aboutNav = document.createElement("li");
   aboutNav.className = "nav-item";
@@ -58,6 +65,8 @@ const renderNav = (title) => {
       break;
   }
 
+  iconLink.append(icon);
+
   aboutNav.append(aboutLink);
   resumeNav.append(resumeLink);
   projectsNav.append(projectsLink);
@@ -68,6 +77,7 @@ const renderNav = (title) => {
   navbar.append(projectsNav);
   navbar.append(contactNav);
 
+  navRow.append(iconLink);
   navRow.append(name);
   navRow.append(navbar);
 
